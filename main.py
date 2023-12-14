cba69de5e0da6faa0ecec6f1d4ed19ef891af94c
IamSonic17 <ilyaborisov@inbox.ru>
2023-12-14 18:39:26 +0500

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
 }
 
-url = "https://championat.com/"
+url = "https://www.championat.com/"
 session = requests.Session()
-push_req = session.get(url, headers=headers)
+      
+try:
+    push_req = session.get(url, headers=headers)
+    if push_req.status_code == 200:
+        soup = bs4(push_req.content, 'html.parser')
+        divs = soup.find_all('div', attrs={'class': 'news-item__content'})
+        for div in divs:
+            title = div.find('a').text
+            link = div.find('a')['href']
+            print(title)
+            print('https://championat.com/' + link)
+    else:
+        print("Error")
+
+except Exception:
+    print("Введен несуществующий URL")
+
+
