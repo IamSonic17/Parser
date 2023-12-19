db84fc24ec076f5227e5c6ccd48545ff02ddc2d7
IamSonic17 <ilyaborisov@inbox.ru>
2023-12-19 12:42:21 +0500

    fix: implement correct export data to csv (only unique rows)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import csv
-from datetime import date
+import pytz
+from datetime import datetime
 import requests
-from bs4 import BeautifulSoup as bs4
+from bs4 import BeautifulSoup as Bs4
 
 
 def launch_parser():
@@ -12,7 +13,8 @@ def launch_parser():
 
     headers = {
         'accept': '*/*',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)\
+         Chrome/119.0.0.0 Safari/537.36'
     }
 
     url = "https://www.championat.com/news/football/1.html?utm_source=button&utm_medium=news"
@@ -21,22 +23,37 @@ def launch_parser():
     try:
         push_req = session.get(url, headers=headers)
         if push_req.status_code == 200:
-            soup = bs4(push_req.content, 'html.parser')
+            soup = Bs4(push_req.content, 'html.parser')
             divs = soup.find_all('div', attrs={'class': 'news-item'})
             for div in divs:
-                date_and_time = str(date.today()) + ' ' + str(div.find('div', attrs={'class': 'news-item__time'}).text)
+                date_today = str(datetime.now(pytz.timezone('Europe/Moscow'))).split()[0]
+                time = str(div.find('div', attrs={'class': 'news-item__time'}).text)
                 title = div.find('a').text
                 link = 'https://championat.com/' + str(div.find('a')['href'])
+                flag = 0
+                with open('data.csv', 'r') as file:
+                    reader = csv.reader(file)
+                    for row in reader:
+                        if title not in row:
+                            flag = 1
+                            continue
+                        else:
+                            flag = 0
+                            break
+                file.close()
 
-                with open(f'data.csv', 'a', newline='', encoding='utf-8') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(
-                        (
-                            date_and_time,
-                            title,
-                            link,
+                if flag == 1:
+                    with open(f'data.csv', 'a', newline='', encoding='utf-8') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(
+                            (
+                                date_today,
+                                time,
+                                title,
+                                link,
+                            )
                         )
-                    )
+                    file.close()
         else:
             print("Error")
 
@@ -45,4 +62,3 @@ def launch_parser():
 
 
 launch_parser()
-
